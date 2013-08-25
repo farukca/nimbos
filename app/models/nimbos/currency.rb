3fe5c5628880b27fb06525b1c880fee28c2561a9
Faruk Celik <farukca@yahoo.com>
2013-08-25 20:24:35 +0300

    Post form losing target parameter

--- app/models/nimbos/currency.rb
+++ app/models/nimbos/currency.rb
@@ -3,5 +3,12 @@ module Nimbos
     set_primary_key = 'code'
     
     attr_accessible :code, :multiplier, :name, :symbol
+
+    validates_presence_of :code
+	  validates_presence_of :name
+	  validates_uniqueness_of :code, :case_sensitive => false
+	  validates_uniqueness_of :name, :case_sensitive => false
+
+	  scope :nameOrdered, order("name")
   end
 end
