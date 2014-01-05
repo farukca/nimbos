61f11c40ca03cafb9d9fbb3932896bc006cd0f47
Faruk Celik <farukca@yahoo.com>
2014-01-05 22:57:17 +0200

    vehicle added to find_target

--- app/controllers/nimbos/concerns/find_target.rb
+++ app/controllers/nimbos/concerns/find_target.rb
@@ -6,17 +6,19 @@ private
 	  if params[:ticket_id]
 	    return Helpdesk::Ticket.find(params[:ticket_id])
 	  elsif params[:ware_id]
-      return Assetim::Ware.find(params[:ware_id])
-    elsif params[:person_id]
-      return Personal::Person.find(params[:person_id])
-    elsif params[:discussion_id]
-      return Nimbos::Discussion.find(params[:discussion_id])
+        return Assetim::Ware.find(params[:ware_id])
+      elsif params[:person_id]
+        return Personal::Person.find(params[:person_id])
+      elsif params[:discussion_id]
+        return Nimbos::Discussion.find(params[:discussion_id])
 	  elsif params[:position_id]
 	    return Logistics::Position.find(params[:position_id])
 	  elsif params[:loading_id]
 	    return Logistics::Loading.find(params[:loading_id])
 	  elsif params[:transport_id]
 	    return Logistics::Transport.find(params[:transport_id])
+	  elsif params[:vehicle_id]
+	    return Fleet::Vehicle.find(params[:vehicle_id])
 	  elsif params[:document_id]
 	    return Arsiv::Document.find(params[:document_id])
 	  else
