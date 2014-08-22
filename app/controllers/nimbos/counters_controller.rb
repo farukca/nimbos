da08080aa5b34b775eb2400842d0756d2bee8653
Ahmet Yilmaz <ayilmaz4646@gmail.com>
2014-08-22 11:31:15 +0300

    params

--- app/controllers/nimbos/counters_controller.rb
+++ app/controllers/nimbos/counters_controller.rb
@@ -8,11 +8,11 @@ module Nimbos
     end
 
     def edit
-    	@counter = Nimbo::Counter.find(params[:id])
+    	@counter = Nimbos::Counter.find(params[:id])
     end
 
     def create
-    	@counter = Nimbo::Counter.new(counter_params)
+    	@counter = Nimbos::Counter.new(counter_params)
     	respond_to do |format|
     		if @counter.save
     			format.html { redirect_to @counter, notice: t("simple_form.default.message.created", model: Nimbo::Counter.model_name.human) }
@@ -27,18 +27,22 @@ module Nimbos
     end
 
     def update
-    	@counter = Nimbo::Counter.find(params[:id])
+    	@counter = Nimbos::Counter.find(params[:id])
     	respond_to do |format|
     		if @counter.update_attributes(counter_params)
-    			format.html { redirect_to @counter, notice: t("simple_form.default.message.update", model: Nimbo::Counter.model_name.human) }
+    			format.html { redirect_to @counter, notice: t("simple_form.default.message.update", model: Nimbos::Counter.model_name.human) }
     			format.json { head :ok}
-    			format.js { flash.now[:notice] = t("simple_form.default.message.update", model: Nimbo::Counter.model_name.human) }
+    			format.js { flash.now[:notice] = t("simple_form.default.message.update", model: Nimbos::Counter.model_name.human) }
     		else
     			format.html { render action: "edit" }
     			format.json { render json: @counter.errors }
     			format.js
             end
         end
+    end
+    private
+    def counter_params
+        params.require(:counter).permit( :prefix, :suffix, :period, :count, :confirmed )
     end
 
   end
